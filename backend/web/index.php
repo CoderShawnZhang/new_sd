c54bebc42aa68092c37f13d46b69726c28d93386
taobao1875 <taobao1875@163.com>
2020-07-23 11:56:44 +0800

    aaa

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,11 +1,6 @@
 <?php
-ini_set('display_errors',1);
-error_reporting(-1);
-defined('YII_ENV_TEST') or define('YII_ENV_TEST',true);
-
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
@@ -18,4 +13,5 @@ $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../config/main.php',
     require __DIR__ . '/../config/main-local.php'
 );
+
 (new yii\web\Application($config))->run();
