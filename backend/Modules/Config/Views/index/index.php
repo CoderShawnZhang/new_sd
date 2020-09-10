cb7fd5fa213b2f1a0c2abac63a8b7068b67df4f4
taobao1875 <taobao1875@163.com>
2020-09-10 14:17:34 +0800

    aaa

--- backend/Modules/Config/Views/index/index.php
+++ backend/Modules/Config/Views/index/index.php
@@ -36,7 +36,32 @@
                 </div>
             </form>
         </div>
-        <div class="layui-tab-item">内容2</div>
+        <div class="layui-tab-item">
+
+            <div class="voiceinator">
+
+                <select name="voice" id="voices">
+                    <option value="">Select A Voice</option>
+                </select>
+
+                <label for="rate">Rate:</label>
+                <input name="rate" type="range" min="0" max="3" value="1" step="0.1">
+
+                <label for="pitch">Pitch:</label>
+                <input name="pitch" type="range" min="0" max="2" step="0.1">
+
+                <textarea name="text">支付宝到账，1个亿</textarea>
+
+                <button id="stop">Stop!</button>
+                <button id="speak">Speak</button>
+            </div>
+
+            <!-------------------------------------------资源文件------------------------------------------->
+            <!--引入javascript-->
+            <?php $this->registerJs($this->render("/javascript/speak.js"),yii\web\View::POS_END); ?>
+            <!--引入css-->
+
+        </div>
         <div class="layui-tab-item">内容3</div>
         <div class="layui-tab-item">内容4</div>
         <div class="layui-tab-item">内容5</div>
